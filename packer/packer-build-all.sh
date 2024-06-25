bb57728b988bb4e6d70d54ffc6bec9d1cb066ac5
root <root@ilak.threatdump.com>
2024-06-25 17:00:28 -0400

    Initial commit

---
+++ packer/packer-build-all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+
+echo "[*] Running packers..."
+packer build -timestamp-ui -var-file private.json win2016.json &
+packer build -timestamp-ui -var-file private.json win10.json &
+packer build -timestamp-ui -var-file private.json graylog.json &
+packer build -timestamp-ui -var-file private.json kali.json &
+
+wait
+
+echo "[+] All of the builds have been completed."
